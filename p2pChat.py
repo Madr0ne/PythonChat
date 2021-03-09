dc0342e1a7088065fd3d68382d278792b12239e0
Liam McCormick <lmccormick22@cornellcollege.edu>
2021-03-08 20:16:13 -0600

    Reworked client class. Added additional threads.

--- p2pChat.py
+++ p2pChat.py
@@ -5,88 +5,111 @@ import time
 from random import randint
 import re
 
-SOCKET = 50001
 
-
-def scan(network_address, port):
+def scan(host_ip, port, host_name):
     threads = []
     con_list = []
+    network_address = re.search('\d+\.\d+', host_ip).group()
 
     def __do_scan(first_three, start):
-        for i in range(start, start + 15):
+        for k in range(start, start + 15):
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            sock.settimeout(0.1)
+            sock.settimeout(0.05)
 
-            if not sock.connect_ex((f'{first_three}.{i}', port)):
+            if not sock.connect_ex((f'{first_three}.{k}', port)):
+                sock.settimeout(None)
+                sock.sendall(f'{host_name} has joined the chat'.encode())
                 con_list.append(sock)
         return
 
+    print('beginning scan')
     for i in range(255):
         for j in range(0, 256, 16):
             t = threading.Thread(target=__do_scan, args=(f'{network_address}.{i}', j))
             threads.append(t)
             t.start()
-
     for thread in threads:
         thread.join()
+    print('scan complete')
     return con_list
 
 
-class Peer:
+class SendThread(threading.Thread):
+    def __init__(self, client):
+        super(SendThread, self).__init__()
+        self.client = client
 
-    def send_msg(self):
+    def run(self):
         while True:
-            message = self.name + ': ' + input("")
-            print(message)
-            for connection in self.connections:
-                connection.send(bytes(message, 'utf-8'))
+            message = input(f'{self.client.name}: ')
+            self.client.broadcast(message)
 
-    def __init__(self, name, connectionsList):
-        self.name = name
-        self.connections = connectionsList
+
+class Client:
+    def __init__(self, ip, port, client_name):
+        self.ip = ip
+        self.port = port
+        self.peer_list = scan(ip, port, client_name)
+        self.name = client_name
+
+    def broadcast(self, message):
+        for peer in self.peer_list:
+            peer.sendall(f'{self.name}: {message}'.encode())
+
+    def start(self):
+        for peer in self.peer_list:
+            receiver = ReceiveThread(peer, self.name)
+            receiver.start()
+
+        send = SendThread(self)
+        send.start()
+
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(('0.0.0.0', SOCKET))
+
+        sock.bind((self.ip, self.port))
         sock.listen(1)
 
-        for connection in self.connections:
-            socketThread = threading.Thread(target=self.handler, args=(connection,))
-            socketThread.daemon = True
-            socketThread.start()
-
-        sendThread = threading.Thread(target=self.send_msg)
-        sendThread.daemon = True
-        sendThread.start()
-
         while True:
-            connection, a = sock.accept()
-            socketThread = threading.Thread(target=self.handler, args=(connection,))
-            socketThread.daemon = True
-            socketThread.start()
-            self.connections.append(connection)
-            print(str(a[0]) + ':' + str(a[1]), " connected.")
+            connection, address = sock.accept()
+            self.peer_list.append(connection)
+            receiver = ReceiveThread(connection, self.name)
+            receiver.start()
 
-    def handler(self, c):
+
+class ReceiveThread(threading.Thread):
+    def __init__(self, connection, name):
+        super(ReceiveThread, self).__init__()
+        self.connection = connection
+        self.name = name
+
+    def run(self):
         while True:
-            data = c.recv(1024)
-            print(str(data, 'utf-8'))
-            if not data:
-                self.connections.remove(c)
-                c.close()
-                break
+            message = self.connection.recv(1024).decode()
+            if message:
+                print(f'\r{message}\n{self.name}: ', end='')
+            else:
+                print('error')
 
 
+def get_host_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
 
-print('hi')
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-hostIP = str(s.getsockname()[0])
-print(hostIP)
-networkAddress = re.search('\d+\.\d+', hostIP).group()
 
-name = input("Enter your name:")
+def __main__():
+    port = 50001
 
-scannedConnections = scan(networkAddress, SOCKET)
+    name = input("Enter name: ")
+    try:
+        client = Client(get_host_ip(), port, name)
+        client.start()
+    except KeyboardInterrupt:
+        sys.exit(0)
 
-peer = Peer(name, scannedConnections, hostIP)
+
+if __name__ == "__main__":
+    __main__()
