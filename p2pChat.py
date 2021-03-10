e6a71779c533935a80d871ca32d77e125120fd22
Panifex01 <79937240+Panifex01@users.noreply.github.com>
2021-03-09 21:17:36 -0600

    Added comments to scan method

--- p2pChat.py
+++ p2pChat.py
@@ -4,35 +4,59 @@ import sys
 import re
 import os
 
-
+# Method to generate a list of connected hosts
 def scan(host_ip, port, host_name):
+    # Create empty lists for threads and connections
     threads = []
     con_list = []
+    
+    # Extract the first two bytes of the IP address
     network_address = re.search('\d+\.\d+', host_ip).group()
 
     def __do_scan(first_three, start):
+        # Loop through each of the 16 IP addresses
         for k in range(start, start + 15):
+            # If the generated address matches the host address,
+            #   don't bother to try and connect to it
             if f'{first_three}.{k}' == host_ip:
                 continue
-
+            
+            # Create a TCP socket with an automatic timeout and allow it to reuse 
+            #  a local socket in the TIME_WAIT state instead of timing out
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             sock.settimeout(0.05)
-
+            
+            # If no error is raised (connect_ex returns 0), remove
+            #   the timeout and add the socket to the connections list
             if not sock.connect_ex((f'{first_three}.{k}', port)):
                 sock.settimeout(None)
                 con_list.append(sock)
         return
 
     print('Beginning network scan...')
+    
+    # Loop through numbers from 0 to 254 for the third byte
+    # NOTE: 255 in this spot is reserved for broadcast addresses
     for i in range(255):
+        # Start at 0, and move by increments of 16 until 240
+        #   to get ranges of 0-15, 16-31, etc. for the fourth byte
         for j in range(0, 256, 16):
+            # For each range, start a new thread that calls __do_scan
+            
+            # Pass the first three bytes (as a formatted string)
+            #   and the start of each 16-address range to __do_scan
             t = threading.Thread(target=__do_scan, args=(f'{network_address}.{i}', j))
+            # Append each thread to the list created earlier
             threads.append(t)
             t.start()
+    
     for thread in threads:
+        # After __do_scan has finished, join each thread to the main thread
         thread.join()
+    
     print('Scan complete.')
+    # Return the complete list of connections to the created Client object
     return con_list
 
 
